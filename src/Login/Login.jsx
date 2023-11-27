92ff9254f58c886ba2bd93b0224b6e2c5df2ea5a
CanbulatOnarok <canbulatonarok@gmail.com>
2023-11-27 21:57:17 +0300

    data.json eklendi

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,28 +1,62 @@
-import React, { useState } from 'react'
+import axios from 'axios';
+import React, { useState } from 'react';
 
 const Login = ({ onFormSwitch }) => {
   const [email, setEmail] = useState('');
   const [pass, setPass] = useState('');
 
+  const getData = async () => {
+
+    const response = await axios.get('http://localhost:3005/users');
+    const users = response.data;
+
+    const userFound = users.find(user => user.email === email && user.password === pass);
+
+    if (userFound) {
+      console.log('Giriş başarılı');
+
+    } else {
+      console.log('Kullanıcı bulunamadı veya şifre hatalı');
+    }
+
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("email:",email,"pasword:",pass);
+    getData(); // Kullanıcıyı kontrol etmek için verileri çek
     setEmail('');
     setPass('');
-  }
+  };
+
   return (
     <div className='auth-form-container'>
-       <h2>Login</h2>
-      <form className='login-form' onSubmit={handleSubmit} >
-        <label htmlFor="email">email</label>
-        <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder='youremail@gmail.com' id='email' name='email' />
-        <label htmlFor="password">password</label>
-        <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder='********' id='password' name='password' />
+      <h2>Login</h2>
+      <form className='login-form' onSubmit={handleSubmit}>
+        <label htmlFor='email'>email</label>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type='email'
+          placeholder='youremail@gmail.com'
+          id='email'
+          name='email'
+        />
+        <label htmlFor='password'>password</label>
+        <input
+          value={pass}
+          onChange={(e) => setPass(e.target.value)}
+          type='password'
+          placeholder='********'
+          id='password'
+          name='password'
+        />
         <button className='btn'>Log In</button>
       </form>
-      <button className='link-btn' onClick={() => onFormSwitch('register')} >Don't have an account? Register here.</button>
+      <button className='link-btn' onClick={() => onFormSwitch('register')}>
+        Don't have an account? Register here.
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- src/register/Register.jsx
+++ src/register/Register.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React from 'react'
 
 import { useState } from "react";
@@ -6,28 +7,44 @@ const Register = ({ onFormSwitch }) => {
   const [email, setEmail] = useState('');
   const [pass, setPass] = useState('');
   const [name, setName] = useState('');
+  const [newUser, setNewUser] = useState([]);
+  
+  const createUser = async (newProfile) => {
 
+    const response = await axios.post('http://localhost:3005/users',newProfile);
+    if(response.status === 201){
+     setNewUser(prev=>[...prev,newProfile])
+
+   }
+
+  }
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("email:",email,"pasword:",pass);
+    createUser({
+      email:email,
+      pass:pass,
+      name:name
+    })
     setEmail('');
     setPass('');
     setName('');
   }
+
+
 
   return (
     <div className='auth-form-container'>
       <h2>Register</h2>
       <form className='register-form' onSubmit={handleSubmit} >
         <label htmlFor="name" >Full name</label>
-        <input value={name} onChange={(e) => setName(e.target.value)} type="name" placeholder='full Name' id='name' name='name' />
+        <input value={name} onChange={(e) => setName(e.target.value)} type="text" placeholder='full Name' id='name' name='name' />
         <label htmlFor="email">email</label>
         <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder='youremail@gmail.com' id='email' name='email' />
-        <label htmlFor="password">password</label>
+        <label htmlFor="password">Password</label>
         <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder='********' id='password' name='password' />
         <button className='btn'>Log In</button>
       </form>
-      <button className='link-btn'  onClick={() => onFormSwitch('login')}>Already have an account? Login here.</button>
+      <button className='link-btn' onClick={() => onFormSwitch('login')}>Already have an account? Login here.</button>
     </div>
   )
 }
